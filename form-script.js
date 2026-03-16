9a4fb737f98657a84ad1b0069f2b299dd18fb113
Alisherjon Turakulov <aturakulov100@gmail.com>
2026-03-16 17:38:43 -0400

    frame client side script with function documentations

---
+++ form-script.js
@@ -0,0 +1,17 @@
+//client-side script for form-creator
+//submitted forms are saved to a database (temporarily data folder)
+
+const assignment = {};
+
+//saves the assignment object
+function saveAssignment(){
+
+}
+
+/**
+ * loads saved assingments from data folder/database
+ * @param credentials if from a database; the credentials for access the database
+ */
+function loadAssignment(credentials = null){
+
+}
