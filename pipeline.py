62d657a251011fd10895286f49a84005d6aab50b
Alisherjon Turakulov <aturakulov100@gmail.com>
2026-03-18 15:54:06 -0400

    add pipeline file and flask requirements

---
+++ pipeline.py
@@ -0,0 +1,14 @@
+#will hold the data pipline that will run using flask to send a list of questions
+#and then check the answers submitted by the user
+import flask
+import render_template
+
+#use pipeline from notebook
+def sendQuestions():
+    return ""
+
+def checkAnswer():
+
+
+#flask routing
+
