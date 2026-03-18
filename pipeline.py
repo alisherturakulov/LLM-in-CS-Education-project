5002f70334ce0cb7d96dde206b12e544932dda09
Alisherjon Turakulov <aturakulov100@gmail.com>
2026-03-18 18:24:19 -0400

    add reuqired imports for pipeline and update directory structure

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,46 @@
 #will hold the data pipline that will run using flask to send a list of questions
 #and then check the answers submitted by the user
-import flask
-import render_template
+from flask import Flask, render_template, request, jsonify
+import time
+import os
+from python-dotenv import load_dotenv
+from openai import OpenAI
+
+#load from .env file
+load_dotenv()
+
+os.environ['OPENAI_API_KEY'] = "your_api_key_here"#will use dotenv
+clientOpenAI = OpenAI()
+
+
+app = Flask(__name__)
 
 #use pipeline from notebook
-def sendQuestions():
+def send_question():
     return ""
 
-def checkAnswer():
-
+def check_answer():
+    return ""
 
 #flask routing
 
+@app.route('/')
+def home():
+    render_template('index.html')#uses jinja template engine
+
+@app.route('/api/get_questions', methods=['GET'])
+def get_questions():
+    data = request.json
+    data.
+    questions = {}#to jsonify and send
+    return jsonify(questions)
+
+@app.route('/api/submit_answers', methods=['POST'])
+def submit_answers():
+    feedback = {} #in the same order as the answers were received
+    return jsonify(feedback)
+
+//host app
+if __name__ == 'main':
+    app.run(debug=True)
+    
