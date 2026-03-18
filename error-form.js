18e36db2e5e9c70dcc1a4b920ec8af62553540be
Alisherjon Turakulov <aturakulov100@gmail.com>
2026-03-18 14:22:45 -0400

    add script file and change names

---
+++ error-form.js
@@ -0,0 +1,38 @@
+//connects to index.html to handle requests to server
+//script is currently in script tag of index.html
+
+   /**
+             * requests questions form the pipeline running on the server and
+             * adds questions with values in the container 
+             * with id values corresponding to index in server response 
+            */
+            function loadQuestions(){
+
+            }
+
+            /**
+             * Submits the error answers to the server pipeline
+             * returns the feedback from the server on the error answer
+             * @return {string} the feedback on the correctness of the user errorAnswer
+            */
+            function submitAnswers(){
+
+            }
+
+            /**
+             * gets the string input value of the errorAnswer field
+             * @return {string} errorAnswer input value from user
+            */
+            function getErrorInput(){
+
+            }
+
+            /**
+             * gets the feedback to the list of answers  from the server
+             * errorAnswer indices are in the same order as the questions were received in
+             * @param {string[]} 
+             * 
+            */
+            function checkErrorAnswers(errorAnswers){
+
+            }
