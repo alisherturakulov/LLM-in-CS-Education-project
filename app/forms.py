d035829132cdd3e84c3d956028571f1d3797f35c
Alisherjon Turakulov <aturakulov100@gmail.com>
2026-03-30 16:20:33 -0400

    write form-creator to match new form structure and add client side script to update number of questions

--- app/forms.py
+++ app/forms.py
@@ -19,13 +19,13 @@ class Signup(FlaskForm):
 
 #assignment creation
 class CreateAssignment(FlaskForm):
-    number_of_questions = SelectField("Number of Questions", validators=[DataRequired()])
+    number_of_questions = IntegerField("Number of Questions", validators=[DataRequired()], render_kw={"id":"num_questions"})
     questions = FieldList(QuestionForm("Question", validators=[DataRequired()]))
     submit = SubmitField("Create Assignment")
 class QuestionForm(FlaskForm):
     class Meta:
         csrf = False#csrf will be handled in parent form
-    question = StringField("Question Text", validators=[DataRequired()])
+    question = StringField("Question Text", validators=[DataRequired()])#error class numbers based on pipeline2 string 'system_prompt_generate1'
     error_type = SelectMultipleField("Error Type", selection=[('0', 'Mental Typo'),( '1', 'Knowledge Gap'),('2', 'Misconception'),('3', 'Wrong Choice'),( '4','Structural Blindness')])
 
 
