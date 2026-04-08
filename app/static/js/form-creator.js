e90260450cd626a3a0aa03aa9e285913022d5a40
Alisherjon Turakulov <aturakulov100@gmail.com>
2026-04-08 12:18:11 -0400

    add script links to form creatro and submit templates

--- app/static/js/form-creator.js
+++ app/static/js/form-creator.js
@@ -1,73 +1,62 @@
 //client-side script for form-creator
-//to do: convert to react
 //submitted forms are saved to a database (temporarily data folder)
-
-//object holds assignment objects
-const assignments = {};
-// window.onload(()=>{
+            //listens and updates based on the number of questions selection field 
+document.addEventListener("DOMContentLoaded", () => {
+    const wrapper = document.querySelector("form#assignment");
+    const number_of_q_selection = wrapper.querySelector("input#number_of_questions");
     
-// });
-// const number_of_questions = 
-//= { firstAssignment:{}, secondAssignment{}, ...}
+    //listen for when number input loses focus
+    number_of_q_selection.addEventListener("change", (e) => {
+        console.log(wrapper.className);
+        let question_fields = wrapper.querySelectorAll(".questionsContainer");
+        console.log(question_fields);
+        
+        const num_questions = parseInt(e.target.value) || 1;//min entries = 1
+        console.log("questions: " + num_questions);
+        if(question_fields.length > num_questions){
+            //remove extra question, left with num_questions count of qs
+            // const keep = prompt(`You have more than ${num_questions}, type 'yes' or 'y' to remove extra questions`);
+            // if(keep?.toLowerCase().includes("yes") || keep?.toLowerCase().includes("y")){
+                for(let i = question_fields.length-1; i>=num_questions; --i){
+                    question_fields[i].remove();
+                }
+            // }else{
+            //     e.target.value = question_fields.length;
+            // }
+        }else{
+            //const lastQuestion = question_fields[question_fields.length-1];
+            for(let i = question_fields.length; i < num_questions; ++i){
+                console.log("calling cloneNode");
+                question_fields = wrapper.querySelectorAll(".questionsContainer");
+                const cloneQ = question_fields[0].cloneNode(true);
+                //console.log(cloneQ.toString());
+                const inputs = cloneQ.querySelectorAll("input, select");
+                let name;
+                let idIndex = 0
+                inputs.forEach((input) => {
+                    //get name
+                    name = input.name?.substring(input.name.lastIndexOf("-"));
+                    if (input.name) input.name = `questions-${i}${name}`;
+                    if (input.id) input.id = `questions-${i}${name}`;
+                    if(input.type && input.type !== "submit" && input.type !== "button"){
+                    input.value = "";
+                    }
+                    ++idIndex
+                });
+                const labels = cloneQ.querySelectorAll("label");
+                idIndex = 0
+                labels.forEach((label)=>{
+                    name = label.name?.substring(label.name.lastIndexOf("-"));
+                    if(label.htmlFor) {
+                        label.htmlFor = `questions-${i}${name}`;
+                    }
+                    ++idIndex
+                });
+                
+                wrapper.querySelector(".questionFieldDiv").append(cloneQ);
+            }
+            };
+    });
+});
 
-//assignment object 
-//{
-// title: "string",
-// description: "string", 
-// answer:"string", 
-// media:"path/to/media in /media or database location"
-//}
-/**
- * 
- * @param {Integer} count
- */
-async function generate_questions(count){
-    const question_data = await fetch("./assign", {
-        method:"GET",
-    }
-    );
-    const questions = question_data.questions;
-    return questions;
-}
 
-/**
- * generates the current assignment and saves into the database
- * @param {object} credentials if from a databse; for accessing
- */
-function generateAssignment(credentials, title){
-
-}
-
-
-/**
- * loads saved assingments from data folder/database into current assignment viewer
- * @param {object} credentials credential obj if from a database; for accessing
- */
-async function loadAssignment(count, credentials=undefined){
-    const questions = await generate_questions(count);
-    const form = document.querySelector("#assignment");
-    for(let i =0; i<questions.size(); ++i){
-        const question = document.createElement("p");
-        question.className = "question";
-        question.textContent = questions[i].question;
-        form.appendChild(question);
-    }
-    
-}
-
-/**
- * remove the specific assignment from list of assignments
- * @param {object} credentials
- * @param {integer} title 
- */
-function removeAssignment(credentials, title){
-
-}
-
-/**
- * removes the question with the given id from the list of questions in the current assignment
- * @param {string} questionId 
- */
-function removeQuestion(questionId){
-
-}
