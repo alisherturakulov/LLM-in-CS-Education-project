cf1e99772653d104eb12fe5b04798fd2c93df8a7
Alisherjon Turakulov <aturakulov100@gmail.com>
2026-04-08 18:42:41 -0400

    start writing highlighting type question handler in submit.js

--- app/static/js/submit.js
+++ app/static/js/submit.js
@@ -1,6 +1,31 @@
 //connects to index.html to handle requests to server
 //script is currently in script tag of index.html
 
+/**
+ * records the part of the question that has been highlighted to put into the stringfield that holds the answers
+ * the highlight answer input types are stringfields but store 
+ * on separate lines the highlighted text in order of appearance in the question
+ */
+function recordHighlight(){
+
+}
+
+//select all answerfields which are string input fields
+answers= document.querySelectorAll("p.erroneousAnswer + input");
+
+//add event listeners to each that check for highlighting
+
+//connect to  recordHighlight function which retrieves the highlighted text
+
+//put each highlight in its own line ordered by index of apearance in the question string
+
+//the input will be uneditable and will update as the user highlights text,
+
+//to remove highlights, directly edit the input field
+
+//students can still type in answers but can only
+//use highlighting when selected by teacher in original form
+
    /**
              * requests questions form the pipeline running on the server and
              * adds questions with values in the container 
